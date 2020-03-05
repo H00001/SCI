c3f4ac9cfb8bc1a30365f38f8e583e80c8d79ae6
hanrenda@megvii.com <hanrenda@megvii.com>
2020-03-05 16:31:08 +0800

    fmt

--- client/main.go
+++ client/main.go
@@ -15,9 +15,9 @@ import (
 	"strings"
 )
 
-func main(){
-	if os.Args[1] == "--upgrade" {
-		fmt.Printf("updating... :%s\n",runtime.GOOS)
+func main() {
+	if len(os.Args) == 2 && os.Args[1] == "--upgrade" {
+		fmt.Printf("updating... :system:%s\n", runtime.GOOS)
 		res, err := http.Get("https://p.gunplan.top/tools/SCI_client_" + runtime.GOOS)
 		if err != nil {
 			log.Fatal(err)
@@ -29,26 +29,27 @@ func main(){
 		}
 		f, _ := os.Create("SCI_client" + suffix)
 		io.Copy(f, res.Body)
+		fmt.Printf("\033[42m;[SUCCEED]\033[0m\n")
 		return
 	}
-	resp,err := http.Get("https://p.gunplan.top/tools/serverlist.html")
-	body,err := ioutil.ReadAll(resp.Body)
+	resp, err := http.Get("https://p.gunplan.top/tools/serverlist.html")
+	body, err := ioutil.ReadAll(resp.Body)
 	s := string(body)
-	conn, err := grpc.Dial(strings.Split(s,"\n")[0], grpc.WithInsecure())
+	conn, err := grpc.Dial(strings.Split(s, "\n")[0], grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("cnnection fail: %v", err)
 	}
 	defer conn.Close()
 	t := connection.NewVerifyClient(conn)
 	fmt.Print("connection server ... \n")
-	tr, err := t.AcquireMagic(context.Background(), &connection.MagicInbound{ Uuid:uuid.NewV4().String()})
-	if err != nil{
-		log.Fatalf("connect error: %v",err)
+	tr, err := t.AcquireMagic(context.Background(), &connection.MagicInbound{Uuid: uuid.NewV4().String()})
+	if err != nil {
+		log.Fatalf("connect error: %v", err)
 	}
-	fmt.Printf("verifying... UUID:%s\n",tr.Magic)
-	token,err :=t.AcquireCalc(context.Background(),&connection.Base64Result{B65:tr.Magic})
+	fmt.Printf("verifying... UUID:%s\n", tr.Magic)
+	token, err := t.AcquireCalc(context.Background(), &connection.Base64Result{B65: tr.Magic})
 	if err != nil {
 		log.Fatalf("could not check sha256: %v", err)
 	}
-	fmt.Printf("connected!:%s\n",token.Token)
+	fmt.Printf("connected!:%s\n", token.Token)
 }
