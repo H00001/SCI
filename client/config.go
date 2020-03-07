3926be81de6a660fe9f2772583ad4851e50ebcfb
hanrenda@megvii.com <hanrenda@megvii.com>
2020-03-07 21:55:48 +0800

    + update Uptime

--- client/config.go
+++ client/config.go
@@ -5,4 +5,5 @@ type Version struct {
 	Md5     string `json:"md5"`
 	Sha1    string `json:"sha1"`
 	Sha256  string `json:"sha256"`
+	UpTime  string `json:"up_time"`
 }
--- client/write_version.sh
+++ client/write_version.sh
@@ -3,7 +3,7 @@ LIB="client"
 FILE="version.json"
 
 make build-linux
-printf '{"version":"%s","md5":"%s","sha1":"%s","sha256":"%s"}' "$(cat version)" "$(md5sum ${LIB}|cut -d" " -f1)" "$(sha1sum ${LIB}|cut -d" " -f1)" "$(sha256sum ${LIB}|cut -d" " -f1)" > $FILE
+printf '{"version":"%s","md5":"%s","sha1":"%s","sha256":"%s","up_time":"%s"}' "$(cat version)" "$(md5sum ${LIB}|cut -d" " -f1)" "$(sha1sum ${LIB}|cut -d" " -f1)" "$(sha256sum ${LIB}|cut -d" " -f1)" "$(date)" > $FILE
 mv client SCI_client_linux
 scp SCI_client_linux root@123.56.223.150:/var/www/physics/static
 scp version.json root@123.56.223.150:/var/www/physics/static
