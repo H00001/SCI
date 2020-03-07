89cefc64bf5fe043d8ebbffa777c6a41a67e8ebc
hanrenda@megvii.com <hanrenda@megvii.com>
2020-03-07 13:07:07 +0800

    + update color 000

---
+++ client/write_version.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+LIB="client"
+FILE="version.json"
+
+go build
+mv client SCI_client_linux
+printf '{"version":"%s","md5":"%s","sha1":"%s","sha256":"%s"}' "$(cat version)" "$(md5sum ${LIB}|cut -d" " -f1)" "$(sha1sum ${LIB}|cut -d" " -f1)" "$(sha256sum ${LIB}|cut -d" " -f1)" > $FILE
+scp SCI_client_linux root@123.56.223.150:/var/www/physics/static
+scp version.json root@123.56.223.150:/var/www/physics/static
